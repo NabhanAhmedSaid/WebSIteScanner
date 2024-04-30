58af8316acaeec24114e910ac4b5f55c1cce6bc0
safcsp dev <dev@test.com>
2024-04-30 12:00:43 +0400

    Add project files.

---
+++ URLCSharp/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Uri link = new Uri("https://google.com");
+Console.WriteLine($" The Port Number {link.Port}");
+Console.WriteLine($" The Id  Host  {link.IdnHost}");
+Console.WriteLine($"The Host :{link.Host}");
+Console.WriteLine($"The Defaul Port  {link.IsDefaultPort}");
+Console.WriteLine($"The Info Of User {link.UserInfo}");
+Console.WriteLine($"Scheme {link.Scheme}");
